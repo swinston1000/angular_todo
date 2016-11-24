7d02deae2fe0351d31eaa44b8d228ad2a0decb2b
Steven Winston <swinston100@hotmail.com>
2016-11-24 14:14:31 +0200

    local starage

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ["ngStorage"])
+    // .run(function($rootScope, $window) {
+    //     $rootScope.$broadcast('restorestate');
+    //     //save on window unload
+    //     angular.element($window).onbeforeunload = function(event) {
+    //         $rootScope.$broadcast('savestate');
+    //     };
+    // })
+
 
 // awesome solution to auto-focus from:
 // http://stackoverflow.com/questions/14833326/how-to-set-focus-on-input-field
@@ -18,50 +26,86 @@ angular.module("myApp").directive('focusMe', function($timeout) {
     };
 });
 
-angular.module("myApp").service('toDoService', function() {
 
-    this.todos = { items: [] };
-    this.addTodo = function(todo) {
-        this.todos.items.push(todo);
+angular.module("myApp").factory('toDoService', function($window, $rootScope, $localStorage) {
+
+    // angular.element($window).on('storage', function(event) {
+    //     alert(event);
+    //     if (event.key === 'todos') {
+    //         $rootScope.$apply();
+    //     }
+    // });
+    // if (!$window.localStorage.getItem('todos')) {
+    //     $window.localStorage.setItem('todos', JSON.stringify({ items: [] }))
+    // }
+    // var todos = JSON.parse($window.localStorage.getItem('todos'));
+
+
+    if (!$localStorage.todos) {
+        $localStorage.todos = { items: [] }
     }
-    this.removeTodo = function(index) {
-        this.todos.items.splice(index, 1);
-        console.log(this.todos);
+
+    var addTodo = function(todo) {
+        $localStorage.todos.items.push(todo);
+        //$window.localStorage.setItem('todos', JSON.stringify(todos))
+    }
+
+    var removeTodo = function(index) {
+        $localStorage.todos.items.splice(index, 1);
+        //$window.localStorage.setItem('todos', JSON.stringify(todos))
     };
-    this.removeAllCompletedToDos = function() {
-        this.todos.items = this.todos.items.filter(function(item) {
+    var removeAllCompletedToDos = function() {
+        $localStorage.todos.items = $localStorage.todos.items.filter(function(item) {
             return !item.completed;
         });
-        //this.todos.splice(1, 1);
-        //console.log(this.todos.items);
+        //$window.localStorage.setItem('todos', JSON.stringify(todos))
+    }
+
+    // $rootScope.$on("savestate", function() {
+    //     $localStorage.steven_todos = todos;
+    // });
+    // $rootScope.$on("restorestate", function() {
+    //     todos = $localStorage.steven_todos;
+    // }());
+
+    return {
+        todos: $localStorage.todos,
+        addTodo: addTodo,
+        removeTodo: removeTodo,
+        removeAllCompletedToDos: removeAllCompletedToDos
     }
 });
 
-angular.module("myApp").controller('appCtrl', function(toDoService) {
-    this.todos = toDoService.todos;
-    this.applyFilter = false;
-    this.addItem = function() {
+angular.module("myApp").controller('appCtrl', function($scope, toDoService) {
+
+    _thisCtrl = this;
+    _thisCtrl.todos = toDoService.todos;
+    _thisCtrl.applyFilter = false;
+    _thisCtrl.addItem = function() {
         if (!this.todoInput) {
             return alert("Please enter an item!");
         }
-        toDoService.addTodo({ task: this.todoInput, completed: false });
-        this.todoInput = ""
+        toDoService.addTodo({ task: _thisCtrl.todoInput, completed: false });
+        _thisCtrl.todoInput = ""
     };
-    this.removeItem = function(index) {
+    _thisCtrl.removeItem = function(index) {
         toDoService.removeTodo(index);
     };
-    this.editItem = function(index) {
-        this.itemToEdit = index;
-        console.log(this.todos);
+    _thisCtrl.editItem = function(index) {
+        _thisCtrl.itemToEdit = index;
     };
-    this.filterComplete = function(_filter) {
+    _thisCtrl.filterComplete = function(item) {
         // if the filter is on/true we need to return false if the item is completed and true if not
         // otherwise, if the filter is off we return true for all items
-        return function(item) {
-            return _filter ? !item.completed : true;
-        };
+        return _thisCtrl.applyFilter ? !item.completed : true;
     };
-    this.removeCompleted = function() {
+
+    _thisCtrl.removeCompleted = function() {
         toDoService.removeAllCompletedToDos();
     };
+
+    $scope.$watch('myCtrl.applyFilter', function(newValue, oldValue) {
+        _thisCtrl.newValue = newValue;
+    });
+
 });
