effa6c447c0411aed2d05e426e59056e4848d39b
Steven Winston <swinston100@hotmail.com>
2016-11-29 20:00:17 +0200

    login and logout working

--- public/components/auth/auth.service.js
+++ public/components/auth/auth.service.js
@@ -1,34 +1,33 @@
-(function() {
+'use strict';
 
-    'use strict';
+angular
+    .module('myApp')
+    .service('authService', authService);
 
-    angular
-        .module('myApp')
-        .service('authService', authService);
+function authService(lock, authManager) {
 
-    function authService(lock, authManager) {
-
-        function login() {
-            lock.show();
-        }
-
-        function logout() {
-            localStorage.removeItem('id_token');
-            authManager.unauthenticate();
-        }
-
-        // Set up the logic for when a user authenticates
-        // This method is called from app.run.js
-        function registerAuthenticationListener() {
-            lock.on('authenticated', function(authResult) {
-                localStorage.setItem('id_token', authResult.idToken);
-                authManager.authenticate();
-            });
-        }
-
-        return {
-            login: login,
-            registerAuthenticationListener: registerAuthenticationListener
-        }
+    function login() {
+        lock.show();
     }
-})();
+
+    function logout() {
+        console.log("obj");
+        localStorage.removeItem('id_token');
+        authManager.unauthenticate();
+    }
+
+    // Set up the logic for when a user authenticates
+    // This method is called from app.run.js
+    function registerAuthenticationListener() {
+        lock.on('authenticated', function(authResult) {
+            localStorage.setItem('id_token', authResult.idToken);
+            authManager.authenticate();
+        });
+    }
+
+    return {
+        login: login,
+        logout: logout,
+        registerAuthenticationListener: registerAuthenticationListener
+    }
+}
