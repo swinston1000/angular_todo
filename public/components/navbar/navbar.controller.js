9767821b249f0d5dcd4fb2206068b8f74389dd2d
Steven Winston <swinston100@hotmail.com>
2016-12-27 11:57:59 +0200

    improved layout

--- public/components/navbar/navbar.controller.js
+++ public/components/navbar/navbar.controller.js
@@ -1,11 +1,21 @@
 'use strict';
 
 angular.module('myApp')
-    .controller('NavbarController', function(authService, $stateParams) {
+    .controller('NavbarController', function($window, authService, $stateParams) {
 
         this.authService = authService;
 
         if (screen.width < 728) {
             this.isNavCollapsed = true;
         }
+
+        this.focus = function() {
+            setTimeout(function() {
+                var element = $window.document.getElementById("navbar-collapse");
+                if (element) {
+                    element.focus();
+                }
+            }, 0);
+        };
+
     });
--- public/components/navbar/navbar.directive.js
+++ public/components/navbar/navbar.directive.js
@@ -7,3 +7,17 @@ angular.module('myApp').directive('navBar', function() {
         templateUrl: '/components/navbar/navbar.template.html'
     };
 });
+
+angular.module('myApp').directive('autoCollapse', function() {
+    return {
+        restrict: 'A',
+        controller: 'NavbarController',
+        link: function(scope, elem, attrs, nbCtrl) {
+            elem.on('focusout', function() {
+                setTimeout(function() {
+                    nbCtrl.isNavCollapsed = true;
+                }, 0)
+            });
+        }
+    };
+});
